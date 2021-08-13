4df4078c44435988e56fbe5464a0db55a42612e6
Your Name <maxime@Massimos-Mac.local>
2021-08-13 17:22:28 +0200

    added places autopcomplete

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+    geocodeByAddress,
+    geocodeByPlaceId,
+    getLatLng,
+  } from 'react-places-autocomplete';
+
 import './Popular.css'
+import PlacesAutocomplete from 'react-places-autocomplete/dist/PlacesAutocomplete';
 
 function Popular() {
+    const [value, setValue] = useState(null);
+
+    const handleSelect = () => {
+
+    }
+
     return (
         <div className='popular'>
             <h2>Popular Places</h2>
+            <PlacesAutocomplete value={value} onChange={setValue} onSelect={handleSelect}>
+                {()=>(<div>Hey</div>)}
+            </PlacesAutocomplete>
         </div>
     )
 }
