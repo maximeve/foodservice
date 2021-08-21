def07d49e1df83c5d56c059a595e1bfbbdcf24f7
maximeve <maxime-vaneijndhoven@hotmail.com>
2021-08-21 15:02:23 +0200

    added caroussel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
         <div>
             <nav className="navbar">
-                <img src="./assets/Foodlogo.png" alt="Logo" className="navbar__logo"/>
+                <Link to='/'>
+                <img src="/assets/Foodlogo.png" alt="Logo" className="navbar__logo"/></Link>
                 <ul>
                     <li>Why X</li>
                     <li>SERVICES</li>
                     <li>CONTACT</li>
                 </ul>
                 <div className="navbar__icons">
-                <img src="./assets/cart.png" alt="cart" className="navbar__cart"/>
-                <img src="./assets/profilepic.png" alt="profile" className="navbar__profile"/>
+                <img src="/assets/cart.png" alt="cart" className="navbar__cart"/>
+                <img src="/assets/profilepic.png" alt="profile" className="navbar__profile"/>
                 </div>
             </nav>
         </div>
--- src/screens/RestaurantDetail.js
+++ src/screens/RestaurantDetail.js
@@ -1,50 +1,70 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-import { RatingView } from 'react-simple-star-rating'
-import './RestaurantDetail.css';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { RatingView } from "react-simple-star-rating";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel'
+import "./RestaurantDetail.css";
+import Navbar from '../components/Navbar';
 
 function RestaurantDetail() {
-    const [restaurant, setRestaurant] = useState('')
-    const [hours, setHours] = useState([])
-    const [images, setImages] = useState([])
-    const [rating, setRating] = useState('')
-    let { id } = useParams();
+  const [restaurant, setRestaurant] = useState("");
+  const [hours, setHours] = useState([]);
+  const [images, setImages] = useState([]);
+  const [rating, setRating] = useState("");
+  const [website,setWebsite] = useState('')
+  let { id } = useParams();
 
-    const handleData = (data) => {
-        setRestaurant(data.result)
-       data.result.photos.map(photo => {
-           images.push(`https://maps.googleapis.com/maps/api/place/photo?photo_reference=${photo.photo_reference}&maxwidth=400&key=${process.env.REACT_APP_GOOLE_MAPS}`)
-       } )
-        if (data.result.opening_hours) {
-            setHours(data.result.opening_hours.weekday_text)
-        } else {
-            setHours([])
-        }
-        setRating(data.result.rating)
+  const handleData = (data) => {
+    console.log(data)
+    setRestaurant(data.result);
+    setWebsite(data.result.website)
+    data.result.photos.map((photo) => {
+      images.push(
+        `https://maps.googleapis.com/maps/api/place/photo?photo_reference=${photo.photo_reference}&maxwidth=400&key=${process.env.REACT_APP_GOOLE_MAPS}`
+      );
+    });
+    if (data.result.opening_hours) {
+      setHours(data.result.opening_hours.weekday_text);
+    } else {
+      setHours([]);
     }
+    setRating(data.result.rating);
+  };
 
-    useEffect(() => {
-        async function fetchData() {
-            await fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=${id}&fields=name,opening_hours,photos,rating,website,reviews,types&key=${process.env.REACT_APP_GOOLE_MAPS}`)
-                .then(response => response.json())
-                .then(data => handleData(data))
-                .catch((error) => {
-                    console.error('Error:', error);
-                })
-        }
-        fetchData()
-    }, [id]);
+  useEffect(() => {
+    async function fetchData() {
+      await fetch(
+        `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=${id}&fields=name,opening_hours,photos,rating,website,reviews,types&key=${process.env.REACT_APP_GOOLE_MAPS}`
+      )
+        .then((response) => response.json())
+        .then((data) => handleData(data))
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+    fetchData();
+  }, [id]);
 
-
-    return (
-        <div>
-            <div className="restaurantdetail">
-            </div>
-            <p>{restaurant.name}</p>
-            <RatingView ratingValue={rating} />
-            {hours.map(day => <p key={day}>{day}</p>)}
-        </div>
-    )
+  return (
+    <React.Fragment>
+    <Navbar/>
+    <div className="restaurantdetail">
+      <div className="restaurantdetail__info">
+        <h2>{restaurant.name}</h2>
+        <RatingView ratingValue={rating} />
+        {hours.map((day) => (
+          <p key={day}>{day}</p>
+        ))}
+        <p className='restaurantdetail__website'><a href={website} target="_blank">website</a></p>
+      </div>
+      <div className="restaurantdetail__img">
+      <Carousel>
+        {images.map(image => <div className="slide__div"><img src={image} /></div> )}
+        </Carousel>
+      </div>
+    </div>
+    </React.Fragment>
+  );
 }
 
-export default RestaurantDetail
+export default RestaurantDetail;
