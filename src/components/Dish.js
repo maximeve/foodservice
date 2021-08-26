6166092ff39c9563794db61a92d66ed05924dbdf
Maxime Van Eijndhoven <30697272+maximeve@users.noreply.github.com>
2021-08-26 16:50:31 +0200

    Add files via upload

--- src/components/Dish.js
+++ src/components/Dish.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './Dish.css'
 
-function Dish() {
-    return (
-        <div className="dish">
-        <div className='dish__details'>
-          <h3>Magherita pizza</h3>
-          <p>Basil leaves,cheesy thin crust</p>
-          <p>&euro; 12</p>
-          <button>ADD TO CART</button>
-        </div>
-        <img src="/assets/Foodlogo.png" className="dish__img"></img>
+
+// NOG EEN ID NODIG PROPS
+
+function Dish(props) {
+
+  const dishName = useRef(null)
+  const dishPrice = useRef(null)
+  const dishPic = useRef(null)
+
+  const addCartHandler = () => {
+    let cartItem = {
+      'dish': dishName.current.innerHTML,
+      'price': dishPrice.current.innerHTML,
+      'photo': dishPic.current.currentSrc
+    }
+  }
+
+  return (
+    <div className="dish">
+      <div className='dish__details'>
+        <h3 ref={dishName} >Magherita pizza</h3>
+        <p>Basil leaves,cheesy thin crust</p>
+        <p ref={dishPrice}>&euro; 12</p>
+        <button onClick={addCartHandler}>ADD TO CART</button>
       </div>
-    )
+      <img src="/assets/Foodlogo.png" className="dish__img" ref={dishPic} />
+    </div>
+  )
 }
 
 export default Dish
