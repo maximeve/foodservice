a95c201a3dee138b0cfb5e66f9de77475e5c4e79
maximeve <maxime-vaneijndhoven@hotmail.com>
2021-09-20 12:47:20 +0200

    footer boilerplate

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <img src="" />
+        <img src="" />
+      </div>
+      <div>
+        <div>
+          <ul>
+            <li>Get Help</li>
+            <li>Add your restaurant</li>
+            <li>Sign up to deliver</li>
+            <li>Create a business account</li>
+            <li>Save on your first order</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Restaurants near me</li>
+            <li>View all cities</li>
+            <li>View all countries</li>
+            <li>Read our blog</li>
+            <li>About Uber Eats</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
