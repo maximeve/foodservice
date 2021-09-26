927c718051d53d7c22c82a872dd1c53be9707b20
Maxime <maxime@massimos-mac.home>
2021-09-26 10:53:30 +0200

    interaction navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { Link } from "react-router-dom";
 import { useDispatch } from 'react-redux';
@@ -10,13 +10,19 @@ function Navbar() {
     const dispatch = useDispatch();
 
     const modalStatecurrent = useSelector((state) => state.cart.modalIsOpen);
+    const [isActive,setisActive ] = useState('')
 
     const modalHandler = () => {
         dispatch(modalState())
     }
 
     const clickHandler = () => {
-        
+        if(isActive === ''){
+            setisActive('active')
+        }else{
+            setisActive('')
+        }
+        console.log(isActive)
     }
 
     return (
@@ -24,12 +30,12 @@ function Navbar() {
             <nav className="navbar">
                 <Link to='/'>
                     <img src="/assets/Foodlogo.png" alt="Logo" className="navbar__logo" /></Link>
-                    <a href="#" className="toggle-button" onClick={clickHandler}>
+                    <span className={`toggle-button ${isActive}`} onClick={clickHandler}>
                     <span className="bar"></span>
                     <span className="bar"></span>
                     <span className="bar"></span>
-                    </a>
-                <ul>
+                    </span>
+                <ul className={isActive}>
                     <li><Link to='/services'>SERVICES</Link></li>
                     <li><Link to='/contact'>CONTACT</Link></li>
                 </ul>
